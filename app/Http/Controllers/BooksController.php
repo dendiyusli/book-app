ff64a86e24d396d7ff05dce94d303c85375f1a62
dendiyusli <dendi@aniqma.com>
2021-01-20 10:52:26 +0700

    added exception where book doesn't exist

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -2,14 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 class BooksController
 {
     public function index()
     {
-        return [
-            ['Judul' => 'Apa Aja'],
-            ['Judul' => 'Boleh Aja']
-        ];
+        // return [
+        //     ['Judul' => 'Apa Aja'],
+        //     ['Judul' => 'Boleh Aja']
+        // ];
+        return Book::all();
+    }
+
+    public function show($id)
+    {
+        try {
+            return Book::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'error' => [
+                    'message' => 'Buku tidak ditemukan !'
+                ]
+            ], 404);
+        }
     }
 }
 
--- tests/app/Http/Controllers/BooksControllerTest.php
+++ tests/app/Http/Controllers/BooksControllerTest.php
@@ -7,18 +7,48 @@ use TestCase;
 class BooksControllerTest extends TestCase
 {
     /** @test **/
-    // public function index_status_code_should_be_200()
-    // {
-    //     $this->get('/books')->seeStatusCode(200);
-    // }
-
-    public function index_should_return_array()
+    public function index_status_code_should_be_200()
     {
-        $this->get('/books')->seeJson([
-            'Judul' => 'Apa Aja'
-        ])->seeJson([
-            'Judul' => 'Boleh Aja'
+        $this->get('/books')->seeStatusCode(200);
+    }
+
+    // public function index_should_return_array()
+    // {
+    //     $this->get('/books')->seeJson([
+    //         'Judul' => 'Apa Aja'
+    //     ])->seeJson([
+    //         'Judul' => 'Boleh Aja'
+    //     ]);
+    // }
+    /** @test **/
+    public function show_should_return_a_valid_book()
+    {
+        $this->get('/books/1')->seeStatusCode(200)->seeJson([
+            'id' => 1,
+            'judul' => 'Menanti Fajar',
+            'deskripsi' => 'afkopfjkgmealgkepkwopgjkqeogjqopg',
+            'pengarang' => 'dendi yusli'
         ]);
+
+        $data = json_decode($this->response->getContent(), true );
+        $this->assertArrayHasKey('created_at', $data);
+        $this->assertArrayHasKey('updated_at', $data);
+    }
+
+    /** @test **/
+    public function show_should_fail_when_the_book_id_does_not_exist()
+    {
+        $this->get('/books/4')->seeStatusCode(404)->seeJson([
+            'error' => [
+                'message' => 'Buku tidak ditemukan !'
+            ]
+        ]);
+    }
+
+    /** @test **/
+    public function show_route_should_not_match_an_invalid_route()
+    {
+        $this->markTestIncomplete('Pending Test');
     }
 }
 
